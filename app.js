0b6d614a3b93648b02ee23f5638481a9619b9446
Carl Husselbee <carl.husselbee@sensis.com.au>
2012-07-09 17:13:44 +1000

    Very basic UI with geolocation support

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/outside', routes.outside);
 
 app.listen('production' === process.env.NODE_ENV ? 80 : 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-
-/*
- * GET home page.
- */
+var http = require('http');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
+};
+
+
+exports.outside = function(req, res) {
+  var lat = req.query.lat;
+  var long = req.query.long;
+  var data = "";
+
+  var sapiRequest = "http://api.sensis.com.au/ob-20110511/test/search?location=" + lat + "," + long + "&key=XXX";
+  http.get(sapiRequest, function(sapiRes) {
+    sapiRes.on('data', function(d) {
+        data += d;
+    });
+    sapiRes.on('end', function(d) {
+        res.render('outside', {lat: lat, long: long, data: JSON.parse(data)});
+    })
+  });
+
 };
